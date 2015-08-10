0fc6263643ecdfe7b78ae0a430d2848b688e5f3a
Pallas <pallasravae@yahoo.com>
2015-08-10 14:23:35 -0600

    ryu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-	
+	$('.ryu').mouseenter(function() {
+    $('.link1bw').hide();
+    $('.link1').show();
+  })
+    .mouseleave(function() {
+    $('.link1').hide();
+    $('.link1bw').show();
+  })
 });
