26c10ed13060970c461199b86c58d87d20b71db9
Pallas <pallasravae@yahoo.com>
2015-09-02 14:08:29 -0600

    add shopping-app

--- js/script.js
+++ js/script.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
     	$('#color').addClass("hide");
     	$('#light').removeClass("hide");
   	})
+  	$('#shopping').mouseenter(function() {
+    	$('#shopping_light').addClass("hide");
+    	$('#shopping_color').removeClass("hide");
+  	})
+    .mouseleave(function() {
+    	$('#shopping_color').addClass("hide");
+    	$('#shopping_light').removeClass("hide");
+  	})
 });
